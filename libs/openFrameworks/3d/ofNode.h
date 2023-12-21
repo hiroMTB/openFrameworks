48a3eb63aa302162b35e2f95989a30aa1c4abfdf
matoba hiroshi <matobahiroshi@gmail.com>
2023-12-21 23:51:15 +0100

    expose orientation too

--- libs/openFrameworks/3d/ofNode.h
+++ libs/openFrameworks/3d/ofNode.h
@@ -489,7 +489,7 @@ private:
 	void onParentScaleChanged(glm::vec3 & scale) {onScaleChanged();}
 
 	//ofParameter<glm::vec3> position;
-	ofParameter<glm::quat> orientation;
+	//ofParameter<glm::quat> orientation;
 	ofParameter<glm::vec3> scale;
 
 	std::array<glm::vec3,3> axis;
@@ -504,5 +504,5 @@ private:
 
 public:
     ofParameter<glm::vec3> position;
-
+    ofParameter<glm::quat> orientation;
 };
